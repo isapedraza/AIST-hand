199b910421fe9623af3b226ed9db9df9ea34ef61
YahelRamirezP <yahelramirezp@gmail.com>
2026-05-21 00:33:48 -0600

    refactor: make dong_math private to loaders subpackage

--- models/latent-retargeting/src/cross_emb/loaders/__init__.py
+++ models/latent-retargeting/src/cross_emb/loaders/__init__.py
@@ -1,4 +1,3 @@
 from .human_loader import HumanLoader, StaticHumanAnchorLoader, DONG_LABELS, TIP_LABELS
 from .robot_loader import RobotLoader, JointSpec
-from .dong_math import dong_run_stage2
 from .sampler import CrossEmbodimentSampler
