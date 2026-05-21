93fff4e126a0f90e8f267f89a32df464f0e771bf
YahelRamirezP <yahelramirezp@gmail.com>
2026-05-21 00:25:25 -0600

    data: promote cross_embodiment_sampler to cross_emb.loaders.sampler

--- models/latent-retargeting/src/cross_emb/loaders/__init__.py
+++ models/latent-retargeting/src/cross_emb/loaders/__init__.py
@@ -1,3 +1,4 @@
 from .human_loader import HumanLoader, StaticHumanAnchorLoader, DONG_LABELS, TIP_LABELS
 from .robot_loader import RobotLoader, JointSpec
 from .dong_math import dong_run_stage2
+from .sampler import CrossEmbodimentSampler
